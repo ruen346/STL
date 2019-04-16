944662777b06159fde4274d55d6e9d048d1c7996
Ruen_note <ruen346@gmail.com>
2019-04-16 15:21:06 +0900

    4/16

--- MemoryMonster/MemoryMonster/소스.cpp
+++ MemoryMonster/MemoryMonster/소스.cpp
@@ -1,63 +1,31 @@
 #include <iostream>
-#include <list>
 #include "MemoryMonster.h"
 #include "save.h"
-#include <fstream>
+#include <vector>
+#include <list>
 #include <string>
+#include <algorithm>
+#include <iterator>
 using namespace std;
 
 // 2장 순차 컨테이너 (sequence container, (homogeneous data type만 담는다))
 //   array : smart 배열, []를 완벽하게 대체한다
-//   vector
+//   vector : dynamic array (STL 간판선수)
 //   deque : double-ended queue, 덱
-// - list : 이후 컨테이너는 원소보다 더 큰 메모리를 사용한다
+//   list : 이후 컨테이너는 원소보다 더 큰 메모리를 사용한다
 //   forward_list
 
-// "소스.cpp"에 있는 단어를 읽어
-// 길이 순으로 정렬한다(default : ascending order)
-// 길이가 4인 단어는 따로 빼서 "길이4.txt" 파일에 저장한다
-// copy로 container에 원소를 읽어오기 - 반복자 어뎁터
+// 키보드에서 단어를 읽어와 역순으로 출력하라
+// ex) 안녕 하세요 -> 하세요 안녕
 
 int main()
 {
-	ifstream in("소스.cpp");
-	
-	list<string> words;
-	istream_iterator<string> p(in);
+	vector<string> v;
 
-	while (p != istream_iterator<string>())
-		words.emplace(words.begin(), *p++);
+	copy(istream_iterator<string>(cin), istream_iterator<string>(), back_inserter(v));
 
-	words.sort([](const string& a, const string& b)
-	{
-		return a.length() < b.length();
-	});
-
-	//길이가 4, 길이가 5인 위치를 찾는다
-	auto pos4 = find_if(words.begin(), words.end(), [](const string& a)
-	{
-		return a.size() == 4;
-	});
-	if (pos4 == words.end())
-		cout << "그럴리가 없는데" << endl;
-	auto pos5 = find_if(words.begin(), words.end(), [](const string& a)
-	{
-		return a.size() == 5;
-	});
-
-	for (const string& str : words)
-		cout << str << endl;
-	cout << endl;
-
-	// 4글자만 찍어줘
-	for (auto i = pos4; i != pos5; ++i)
+	for (auto i = v.crbegin(); i < v.crend(); ++i)
 		cout << *i << endl;
 
-	list<string> words_4;
-	words_4.splice(words_4.begin(), words, pos4, pos5);
-
-	ofstream out("길이4.txt");
-	copy(words_4.begin(), words_4.end(), ostream_iterator<string>(out, "\n"));
-
-	//save("소스.cpp");
+	save("소스.cpp");
 }
