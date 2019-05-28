4f35fe04b1ff5acacb6fd3dd88d13686b137050a
Ruen_note <ruen346@gmail.com>
2019-05-29 01:11:12 +0900

    5/29

--- MemoryMonster/MemoryMonster/소스.cpp
+++ MemoryMonster/MemoryMonster/소스.cpp
@@ -8,23 +8,28 @@ using namespace std;
 #include <iomanip>
 #include <random>
 #include <string>
+#include <list>
 
-// 검색
+// 7. 다양한 알고리즘
+//    - 이 알고리즘은 이름만 읽어봐도 기능을 알 수 있다.
 
-// find - 
-// find_if -
-// find_if_not - 
-// find_first_of - 
-// adjacent_find - 
-// find_end -
-
-// search - 
-// search_n - 
-
-
+template <class Iter>
+int my_distance(Iter b, Iter e)
+{
+	int cnt();
+	while (b++ != e)
+		cnt++;
+	return cnt;
+}
 
 int main()
 {
+	vector<int> v{ 1,2,3,4,5 };
+
+	cout << "v의 갯수는 " << my_distance(v.begin(), v.end()) << endl;
+
+	list<int> l{ 1,2,3,4,5 };
+	cout << "l의 갯수는 " << my_distance(l.begin(), l.end()) << endl;
 
 	save("소스.cpp");
 }
